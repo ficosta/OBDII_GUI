30185a286727dc16d741116faec4d82201b12c8a
JBAnderson <anderson.jeffreyb@gmail.com>
2014-01-01 16:15:52 -0800

    Program now loops; Button now Label; Sized changed to match screen

--- obdGUI.py
+++ obdGUI.py
@@ -1,9 +1,10 @@
 from Tkinter import *
 import serial
 import time
+import random
 root = Tk()
 #The window size
-#root.geometry("800x600")
+root.geometry("320x240")
 
 def getRPM():
     #ser = serial.Serial('/dev/ttyUSB0',
@@ -17,13 +18,19 @@ def getRPM():
     #serial.write("010C\r")
     #time.sleep(.15)
     #data = ser.readline()
-    console.insert(INSERT, "Test data" + "\n")
+    console.delete(1.0, END)
+    console.insert(INSERT, str(random.randint(1000, 10000)))
+    #console.insert(INSERT, str(data))
     #ser.close()
+    root.after(500, getRPM)
+
 #Widget creation
-console = Text(root, width=50, height=20)
-bu = Button(root, text="GetRPM", command=getRPM)
+console = Text(root, width=5, height=1, font=("Helvetica", 32))
+bu = Label(root, text="RPM")
+
 #Widget layout
 console.grid(row=0, column=0, columnspan=3)
 bu.grid(row=1, column=1, padx=5, pady=5)
 
+root.after(500, getRPM)
 root.mainloop()
