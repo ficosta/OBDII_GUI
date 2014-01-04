afe0bddb44b924ddc1fc29da3e289edbcdbd428a
JBAnderson <anderson.jeffreyb@gmail.com>
2014-01-04 10:33:18 -0800

    Added Oil Temp and test data

--- obdGUI.py
+++ obdGUI.py
@@ -48,6 +48,25 @@ def getMPH():
     #ser.close()
     root.after(500, getMPH)
 
+def getOilTemp():
+    #ser = serial.Serial('/dev/ttyUSB0',
+    #                    baudrate = 38400,
+    #                    parity = serial.PARITY_NONE,
+    #                    stopbits = serial.STOPBITS_ONE,
+    #                    bytesize = serial.EIGHTBITS,
+    #                    timeout = 0)
+    #ser.flushInput()
+    #ser.flushOutput()
+    #serial.write("010C\r")
+    #time.sleep(.15)
+    #data = ser.readline()
+    degree_sign= u'\N{DEGREE SIGN}'
+    console3.delete(1.0, END)
+    console3.insert(INSERT, str(random.randint(0, 550)) + degree_sign)
+    #console.insert(INSERT, str(data))
+    #ser.close()
+    root.after(500, getOilTemp)
+
 #RPM Widget creation
 console = Text(root, width=5, height=1, font=("Helvetica", 64))
 label = Label(root, text="RPM", font=("Helvetica", 32))
@@ -64,7 +83,16 @@ label2 = Label(root, text="MPH", font=("Helvetica", 32))
 console2.grid(row=0, column=4, columnspan=3)
 label2.grid(row=1, column=5, padx=5, pady=5)
 
+#Oil Temp Widget creation
+console3 = Text(root, width=4, height=1, font=("Helvetica", 64))
+label3 = Label(root, text="Oil Temp", font=("Helvetica", 32))
+
+#Oil Temp Widget layout
+console3.grid(row=0, column=7, columnspan=4)
+label3.grid(row=1, column=9, padx=5, pady=5)
+
 root.after(500, getRPM)
 root.after(500, getMPH)
+root.after(500, getOilTemp)
 
 root.mainloop()
